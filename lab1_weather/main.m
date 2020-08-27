ed4536ebc226e0f1a861269aabda3f988c495a8a
caharper <caharper23@gmail.com>
2020-08-27 15:26:31 -0500

    Initial Commit

---
+++ lab1_weather/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  lab1_weather
+//
+//  Created by Clay Harper on 8/27/20.
+//  Copyright © 2020 Mobile Sensing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
