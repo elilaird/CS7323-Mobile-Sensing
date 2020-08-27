61c8ae93a4c3d12b7337aaa1a20e1395f1b38632
caharper <caharper23@gmail.com>
2020-08-27 15:26:31 -0500

    Initial Commit

---
+++ lab1_weather/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  lab1_weather
+//
+//  Created by Clay Harper on 8/27/20.
+//  Copyright © 2020 Mobile Sensing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ lab1_weather/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  lab1_weather
+//
+//  Created by Clay Harper on 8/27/20.
+//  Copyright © 2020 Mobile Sensing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
