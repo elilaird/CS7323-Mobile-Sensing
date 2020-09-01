28e8042996552fde6d8c3c2714ea41529f57e766
caharper <caharper23@gmail.com>
2020-09-01 12:25:41 -0500

    added dictionary functionality

--- lab1_weather/WeatherAPI.m
+++ lab1_weather/WeatherAPI.m
@@ -18,27 +18,46 @@
 
 //* Private Facing Functions *
 
-- (NSString *) getDataFor:(NSString *)location{
-    __block NSString *jsonData = nil;
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    [request setHTTPMethod:@"GET"];
+- (NSDictionary *) getDataFor:(NSString *)location{
+//    __block NSString *jsonData = nil;
+//    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+//    [request setHTTPMethod:@"GET"];
+//
+//    NSString *baseUrl = @"https://api.openweathermap.org/data/2.5/weather";
+//    NSString *appid = @"dd12273b23df1d19ad59652762894830";
+//    NSString *targetUrl = [NSString stringWithFormat:@"%@?q=%@&appid=%@", baseUrl, location, appid];
+//
+//    NSLog(@"%@", targetUrl);
+//
+//    [request setURL:[NSURL URLWithString:targetUrl]];
+//
+//    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:
+//      ^(NSData * _Nullable data,
+//        NSURLResponse * _Nullable response,
+//        NSError * _Nullable error) {
+//
+//          jsonData = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//          NSLog(@"Data received: %@", jsonData);
+//    }] resume];
+    
+//    new code
     
     NSString *baseUrl = @"https://api.openweathermap.org/data/2.5/weather";
     NSString *appid = @"dd12273b23df1d19ad59652762894830";
     NSString *targetUrl = [NSString stringWithFormat:@"%@?q=%@&appid=%@", baseUrl, location, appid];
-    
-    NSLog(@"%@", targetUrl);
-    
-    [request setURL:[NSURL URLWithString:targetUrl]];
-    
-    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:
-      ^(NSData * _Nullable data,
-        NSURLResponse * _Nullable response,
-        NSError * _Nullable error) {
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:targetUrl]];
 
-          jsonData = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-          NSLog(@"Data received: %@", jsonData);
-    }] resume];
+    __block NSDictionary *jsonData = nil;
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                               jsonData = [NSJSONSerialization JSONObjectWithData:data
+                                                                      options:0
+                                                                        error:nil];
+                               NSLog(@"Async JSON: %@", jsonData);
+                           }];
+    
+    
     
     return jsonData;
 }
