2d069d6e88d385b11cbb905236a2cacc1b50bb88
caharper <caharper23@gmail.com>
2020-12-11 10:12:44 -0600

    added interfaces for saving/loading data for all components

--- common_sense/common_sense/ViewController.swift
+++ common_sense/common_sense/ViewController.swift
@@ -12,13 +12,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let di = DataInterface()
-        di.saveTremorData(tremorMagnitude: 5.0)
-        di.saveTremorData(tremorMagnitude: 8.0)
-        let data = di.getTremorData()
-        let elem = data[0]
-        
-        print("Got saved value of \(elem.tremorMagnitude) at time \(elem.timeRecorded)")
     }
 
 
