8c640ba76ad8df27514ba0948ba24d7748af36e6
Matthew Lee <leemh@smu.edu>
2020-12-11 14:44:30 -0600

    made new UI project

---
+++ common_sense_UI/common_sense/FoldingCellsViewController.swift
@@ -0,0 +1,121 @@
+//
+//  FoldingCellsView.swift
+//  common_sense
+//
+//  Created by Matthew Lee on 12/7/20.
+//
+
+import UIKit
+import FoldingCell
+
+class FoldingCellsViewController: UITableViewController {
+
+    enum Const {
+        static let closeCellHeight: CGFloat = 179
+        static let openCellHeight: CGFloat = 495
+        static let rowsCount = 10
+    }
+    
+    var cellHeights: [CGFloat] = []
+    
+    var cells: [String] = ["Hearing", "Vision", "Tremors"]
+
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+
+    // MARK: Helpers
+    private func setup() {
+        cellHeights = Array(repeating: Const.closeCellHeight, count: Const.rowsCount)
+        tableView.estimatedRowHeight = Const.closeCellHeight
+        tableView.rowHeight = UITableView.automaticDimension
+        //tableView.backgroundColor = UIColor(patternImage: #imageLiteral(resourceName: "background"))
+        if #available(iOS 10.0, *) {
+            tableView.refreshControl = UIRefreshControl()
+            tableView.refreshControl?.addTarget(self, action: #selector(refreshHandler), for: .valueChanged)
+        }
+    }
+    
+    // MARK: Actions
+    @objc func refreshHandler() {
+        let deadlineTime = DispatchTime.now() + .seconds(1)
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: { [weak self] in
+            if #available(iOS 10.0, *) {
+                self?.tableView.refreshControl?.endRefreshing()
+            }
+            self?.tableView.reloadData()
+        })
+    }
+}
+
+// MARK: - TableView
+
+extension FoldingCellsViewController {
+
+    override func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return 3
+    }
+
+    override func tableView(_: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard case let cell as SensePreviewCell = cell else {
+            return
+        }
+
+        cell.backgroundColor = .clear
+
+        if cellHeights[indexPath.row] == Const.closeCellHeight {
+            cell.unfold(false, animated: false, completion: nil)
+        } else {
+            cell.unfold(true, animated: false, completion: nil)
+        }
+
+        //cell.number = indexPath.row
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FoldingCell", for: indexPath) as! SensePreviewCell
+        let durations: [TimeInterval] = [0.26, 0.2, 0.2]
+        cell.durationsForExpandedState = durations
+        cell.durationsForCollapsedState = durations
+        cell.lastCheckupLabel.text = cells[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cellHeights[indexPath.row]
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let cell = tableView.cellForRow(at: indexPath) as! FoldingCell
+
+        if cell.isAnimating() {
+            return
+        }
+
+        var duration = 0.0
+        let cellIsCollapsed = cellHeights[indexPath.row] == Const.closeCellHeight
+        if cellIsCollapsed {
+            cellHeights[indexPath.row] = Const.openCellHeight
+            cell.unfold(true, animated: true, completion: nil)
+            duration = 0.5
+        } else {
+            cellHeights[indexPath.row] = Const.closeCellHeight
+            cell.unfold(false, animated: true, completion: nil)
+            duration = 0.8
+        }
+
+        UIView.animate(withDuration: duration, delay: 0, options: .curveEaseOut, animations: { () -> Void in
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+            // fix https://github.com/Ramotion/folding-cell/issues/169
+            if cell.frame.maxY > tableView.frame.maxY {
+                tableView.scrollToRow(at: indexPath, at: UITableView.ScrollPosition.bottom, animated: true)
+            }
+        }, completion: nil)
+    }
+
+}
---
+++ common_sense_UI/common_sense/SensePreviewCell.swift
@@ -0,0 +1,66 @@
+//
+//  SensePreviewCell.swift
+//  common_sense
+//
+//  Created by Matthew Lee on 12/8/20.
+//
+import FoldingCell
+import UIKit
+import Charts
+
+
+class SensePreviewCell: FoldingCell {
+
+    @IBOutlet weak var lastCheckupLabel: UILabel!
+    @IBOutlet weak var senseIcon: UIImageView!
+    @IBOutlet weak var expandedHeader: UILabel!
+    @IBOutlet weak var lineChart: LineChartView!
+    
+
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        foregroundView.layer.cornerRadius = 10
+        foregroundView.layer.masksToBounds = true
+        
+        containerView.layer.cornerRadius = 10
+        containerView.layer.masksToBounds = true
+        
+        AlertIcon.image!.withRenderingMode(.alwaysTemplate)
+        AlertIcon.tintColor = .red
+        
+        var lineChartEntry = [ChartDataEntry]()
+        for i in stride(from: 0, to: 50, by: 5){
+            let value = ChartDataEntry(x: Double(i), y: Double(i))
+            lineChartEntry.append(value)
+        }
+        
+        let line1 = LineChartDataSet(entries: lineChartEntry, label: "Number")
+        line1.colors = [NSUIColor.blue]
+        let data = LineChartData()
+        data.addDataSet(line1)
+        lineChart.data = data
+        lineChart.chartDescription?.text = "Yeet"
+        
+        
+        // Initialization code
+    }
+    
+    
+    
+
+    @IBOutlet weak var AlertIcon: UIImageView!
+    
+    override func animationDuration(_ itemIndex: NSInteger, type _: FoldingCell.AnimationType) -> TimeInterval {
+        let durations = [0.26, 0.2, 0.2]
+        return durations[itemIndex]
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
