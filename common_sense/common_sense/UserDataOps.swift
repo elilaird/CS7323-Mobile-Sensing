1244f96418534766a51119993ff3f758f11b15d5
caharper <caharper23@gmail.com>
2020-12-11 09:54:23 -0600

    saving and loading data properly for tremors

--- common_sense/common_sense/UserDataOps.swift
+++ common_sense/common_sense/UserDataOps.swift
@@ -11,20 +11,62 @@ import Foundation
 class DataInterface {
     
     let userDefaults = UserDefaults.standard
+    let tremorDataKey = "tremorData"
+    let perceptibilityKey = "perceptibilityData"
+    let audioKey = "audioData"
+    
+    func getTremorData() -> [TremorDataElement]{
+        let decoded = userDefaults.data(forKey: tremorDataKey) ?? nil
+        var savedTremorData: [TremorDataElement] = []
+        if decoded != nil {
+            savedTremorData = NSKeyedUnarchiver.unarchiveObject(with: decoded!) as! [TremorDataElement]
+        }
+        
+        return savedTremorData
+    }
     
     func saveTremorData(tremorMagnitude: Float){
         // Get the current date
         let currentDateTime = Date()
         
         // Make a tremor data element
+        let newTremorElement = TremorDataElement(tremorMagnitude: tremorMagnitude, timeRecorded: currentDateTime)
         
         // See if other data has been saved, if so load and append
+//        let savedTremorData = userDefaults.array(forKey: tremorDataKey) as? [TremorDataElement] ?? [TremorDataElement]()
+//        let decoded = userDefaults.data(forKey: tremorDataKey) ?? nil
+//        var savedTremorData: [TremorDataElement] = []
+//        if decoded != nil {
+//            savedTremorData = NSKeyedUnarchiver.unarchiveObject(with: decoded!) as! [TremorDataElement]
+//        }
+        
+        let savedTremorData = getTremorData()
+        
+        print("Saved tremor data: \(savedTremorData)")
+        print("Adding tremor element: \(newTremorElement)")
+        var dataArray = [newTremorElement]
+        
+        if savedTremorData.count > 0{
+            // Make new data array
+            dataArray = savedTremorData + [newTremorElement]
+            
+            // Delete the currently saved data
+            userDefaults.removeObject(forKey: tremorDataKey)
+        }
+        
+        // Encode the data as data (adapted from: https://stackoverflow.com/questions/29986957/save-custom-objects-into-nsuserdefaults)
+        let encoded: Data = NSKeyedArchiver.archivedData(withRootObject: dataArray)
         
         // Save array
+        userDefaults.set(encoded, forKey: tremorDataKey)
+        userDefaults.synchronize()
     }
     
     func removeAllTremorData(){
-        
+        // Delete the currently saved data
+        if userDefaults.bool(forKey: tremorDataKey){
+            userDefaults.removeObject(forKey: tremorDataKey)
+        }
     }
 }
 
--- common_sense/common_sense/ViewController.swift
+++ common_sense/common_sense/ViewController.swift
@@ -12,6 +12,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let di = DataInterface()
+        di.saveTremorData(tremorMagnitude: 5.0)
+        di.saveTremorData(tremorMagnitude: 8.0)
+        let data = di.getTremorData()
+        let elem = data[0]
+        
+        print("Got saved value of \(elem.tremorMagnitude) at time \(elem.timeRecorded)")
     }
 
 
